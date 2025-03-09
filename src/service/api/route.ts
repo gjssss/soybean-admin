fe4d452838a4244c5f9565f62dd8d13ab109f1e0
gjssss <gaojinsong2010@gmail.com>
2025-03-09 20:52:07 +0800

    feat: add route api

--- src/service/api/route.ts
+++ src/service/api/route.ts
@@ -5,9 +5,47 @@ export function fetchGetConstantRoutes() {
   return request<Api.Route.MenuRoute[]>({ url: '/route/getConstantRoutes' });
 }
 
+function convertAToMenuRoute(menu: any): Api.Route.MenuRoute {
+  return {
+    id: menu.id,
+    name: menu.routeName,
+    path: menu.routePath,
+    component: menu.component,
+    meta: {
+      activeMenu: menu.activeMenu,
+      buttons: menu.button ? menu.button.map((b: { code: any; desc: any }) => ({ code: b.code, desc: b.desc })) : [],
+      constant: menu.constant,
+      fixedIndexInTab: menu.fixedIndexInTab,
+      hideInMenu: menu.hideInMenu,
+      href: menu.href,
+      i18nKey: menu.i18nKey,
+      icon: menu.icon,
+      keepAlive: menu.keepAlive,
+      localIcon: menu.localIcon,
+      multiTab: menu.multiTab,
+      order: menu.order,
+      query: menu.query ? menu.query.map((q: { key: any; value: any }) => ({ key: q.key, value: q.value })) : [],
+      title: menu.menuName
+    },
+    children: menu.children ? menu.children.map(convertAToMenuRoute) : []
+  };
+}
+
 /** get user routes */
-export function fetchGetUserRoutes() {
-  return request<Api.Route.UserRoute>({ url: '/route/getUserRoutes' });
+export async function fetchGetUserRoutes(): Promise<{
+  data: Api.Route.UserRoute;
+  error: any;
+}> {
+  const { data, error } = await request({ url: '/route/getUserRoutes' });
+  console.log(data);
+
+  return {
+    data: {
+      routes: data.map(convertAToMenuRoute),
+      home: 'home'
+    },
+    error
+  };
 }
 
 /**
